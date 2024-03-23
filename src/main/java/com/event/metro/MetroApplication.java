59207749d270bb39748c4fcd113999a49b12c2b4
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-23 18:54:14 +0800

    Refactor application

--- src/main/java/com/event/metro/MetroApplication.java
+++ src/main/java/com/event/metro/MetroApplication.java
@@ -1,24 +1,12 @@
 package com.event.metro;
 
-import com.event.metro.repository.RoleRepository;
-import com.event.metro.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MetroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MetroApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        return args -> {
-        };
     }
 }
