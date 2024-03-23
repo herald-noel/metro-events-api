e609512f862fe139abad56938d231ca359c862b4
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-24 03:40:49 +0800

    Implement changing of role

--- src/main/java/com/event/metro/controller/AdminController.java
+++ src/main/java/com/event/metro/controller/AdminController.java
@@ -1,14 +1,24 @@
 package com.event.metro.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.event.metro.model.User;
+import com.event.metro.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admins")
 public class AdminController {
+    @Autowired
+    AdminService adminService;
+
     @GetMapping("/")
     public String helloAdminController() {
         return "Admin access level.";
+    }
+
+    @GetMapping("/{id}/{role}")
+    public ResponseEntity<User> promoteUser(@PathVariable String id, @PathVariable String role) {
+        return adminService.promoteUser(id, role);
     }
 }
---
+++ src/main/java/com/event/metro/service/AdminService.java
@@ -0,0 +1,35 @@
+package com.event.metro.service;
+
+import com.event.metro.model.Role;
+import com.event.metro.model.User;
+import com.event.metro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminService {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+
+    public ResponseEntity<User> promoteUser(String userId, String newAuthority) {
+        User user = userService.findById(userId);
+        List<Role> authorities = new ArrayList<>();
+        for (GrantedAuthority grantedAuthority : user.getAuthorities()) {
+            if (grantedAuthority instanceof Role) {
+                authorities.add((Role) grantedAuthority);
+            }
+        }
+        if (!authorities.isEmpty()) {
+            authorities.get(0).setAuthority(newAuthority.toUpperCase());
+        }
+        User updatedUser = userRepository.save(user);
+        return ResponseEntity.ok(updatedUser);
+    }
+}
