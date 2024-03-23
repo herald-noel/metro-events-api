93c5b788012778886738b1ae29ac8dc95ef73fc5
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-23 13:06:14 +0800

    Implement authentication service

--- src/main/java/com/event/metro/service/AuthenticationService.java
+++ src/main/java/com/event/metro/service/AuthenticationService.java
@@ -1,17 +1,18 @@
 package com.event.metro.service;
 
+import com.event.metro.model.dto.LoginResponseDTO;
 import com.event.metro.model.dto.SignupDTO;
 import com.event.metro.model.ApplicationUser;
-import com.event.metro.model.Role;
 import com.event.metro.repository.RoleRepository;
 import com.event.metro.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashSet;
-import java.util.Set;
 
 
 @Service
@@ -24,10 +25,25 @@ public class AuthenticationService {
     @Autowired
     PasswordEncoder passwordEncoder;
     @Autowired
-    UserService userService;
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
 
     public ApplicationUser signup(SignupDTO dto) {
         ApplicationUser user = new ApplicationUser(dto.getUsername(), dto.getEmail(), passwordEncoder.encode(dto.getPassword()));
         return userRepository.save(user);
+    }
+
+    public LoginResponseDTO loginUser(String username, String password) {
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+
+            String token = tokenService.generateJwt(auth);
+            return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+        } catch (AuthenticationException e) {
+            return new LoginResponseDTO(null, "");
+        }
     }
 }
