4a2128dafcefe38205fc07f873f2736d40910cd6
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-24 15:25:53 +0800

    Implement organizer adding of events

--- src/main/java/com/event/metro/model/Event.java
+++ src/main/java/com/event/metro/model/Event.java
@@ -3,35 +3,15 @@ package com.event.metro.model;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 @Getter
 @Setter
 public class Event {
-    @Id
-    String eventId;
     @NonNull String title;
-    @NonNull LocalDateTime eventDateTimeCreated;
-    @NonNull LocalTime timeStart;
-    @NonNull LocalTime timeEnd;
-    @NonNull LocalDate dateStart;
-    @NonNull LocalDate dateEnd;
+    @NonNull String eventDateTimeCreated;
+    @NonNull String timeStart;
+    @NonNull String timeEnd;
+    @NonNull String dateStart;
+    @NonNull String dateEnd;
     @NonNull String description;
-
-    public Event(String eventId, @NonNull String title, @NonNull LocalDateTime eventDateTimeCreated,
-                 @NonNull LocalTime timeStart, @NonNull LocalTime timeEnd, @NonNull LocalDate dateStart,
-                 @NonNull LocalDate dateEnd, @NonNull String description) {
-        this.eventId = eventId;
-        this.title = title;
-        this.eventDateTimeCreated = eventDateTimeCreated;
-        this.timeStart = timeStart;
-        this.timeEnd = timeEnd;
-        this.dateStart = dateStart;
-        this.dateEnd = dateEnd;
-        this.description = description;
-    }
 }
--- src/main/java/com/event/metro/service/OrganizerService.java
+++ src/main/java/com/event/metro/service/OrganizerService.java
@@ -6,7 +6,8 @@ import com.event.metro.repository.OrganizerEventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class OrganizerService {
@@ -15,12 +16,14 @@ public class OrganizerService {
     OrganizerEventRepository organizerEventRepository;
 
     public ResponseEntity<OrganizerEvents> addOrganizerEvent(String username, Event newEvent) {
-        OrganizerEvents organizerEvents = organizerEventRepository.findByUsername(username).orElseThrow(null);
-        if (organizerEvents != null) {
-            organizerEvents.getEvents().add(newEvent);
-            OrganizerEvents updated = organizerEventRepository.save(organizerEvents);
-            return ResponseEntity.ok(updated);
+        Optional<OrganizerEvents> optionalOrganizerEvents = organizerEventRepository.findByUsername(username);
+        if (optionalOrganizerEvents.isEmpty()) {
+            organizerEventRepository.save(new OrganizerEvents(username));
+            optionalOrganizerEvents = organizerEventRepository.findByUsername(username);
         }
-        return ResponseEntity.notFound().build();
+        OrganizerEvents organizerEvents = optionalOrganizerEvents.orElseThrow();
+        organizerEvents.getEvents().add(newEvent);
+        OrganizerEvents updated = organizerEventRepository.save(organizerEvents);
+        return ResponseEntity.ok(updated);
     }
 }
