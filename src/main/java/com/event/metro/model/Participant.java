7e26f476842997f08b083f6c05d2b291efe2ae0e
herald-noel <bontilao.heraldnoel@gmail.com>
2024-03-24 22:28:44 +0800

    Add status property for event user request

--- src/main/java/com/event/metro/model/Participant.java
+++ src/main/java/com/event/metro/model/Participant.java
@@ -7,4 +7,10 @@ import lombok.Setter;
 @Setter
 public class Participant {
     String username;
+    /*
+     * 0 - For Approval
+     * 1 - Approve
+     * 2 - Decline
+     */
+    int status;
 }
